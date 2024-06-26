197168926cb21b197d763e2491bdd20bff74d6c0
Hadi ElNounou <hadi.elnounou@migrosonline.ch>
2024-06-26 10:02:21 +0200

    Fix it

--- src/geojson/geojson.service.spec.ts
+++ src/geojson/geojson.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
-import { GeojsonService } from './geojson.service';
+import {GeoJsonService} from "./geojson.service";
 
 describe('GeojsonService', () => {
   let service: GeojsonService;
